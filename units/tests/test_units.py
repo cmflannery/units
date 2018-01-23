a52fbfe0b4730a39c1da9804907cc380df30e4d1
Cameron Flannery <cameronflannery@Camerons-MacBook-Pro.local>
2018-01-23 11:07:09 -0800

    adding testing capability

---
+++ units/tests/test_units.py
@@ -0,0 +1,6 @@
+from __future__ import division, absolute_import, print_function
+from units import *
+
+# add openrocketengine to env. variables so we can import openrocketengine here
+def test_Value():
+    thing = Value(100,'kg')
