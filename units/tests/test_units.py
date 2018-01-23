2f3e819a9b9fe7d44834607575524a8eca5a5e4d
Parker Brown <pthb35@gmail.com>
2018-01-23 13:09:05 -0800

    test cases

--- units/tests/test_units.py
+++ units/tests/test_units.py
@@ -4,3 +4,9 @@ from units import *
 # add openrocketengine to env. variables so we can import openrocketengine here
 def test_Value():
     thing = Value(100,'kg')
+
+class TestImperial(object):
+    def test_add(self):
+        a = Value(1, ['m','s'])
+        b = Value(2, ['m','s'])
+        
